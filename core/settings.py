8aff680e86241ad2be2e4a4e88014b20777b07c3
isyormi-IT2 <it2@k2con.com>
2025-05-05 14:22:00 -0500

    todos los host se pueden conectar

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d+wm)dd!-=(o06$7a_mw5zc&4yplk8)a#cgdx&05gtnaifb2(^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
